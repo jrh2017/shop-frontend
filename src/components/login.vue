947ba3741e84f7c5e4e96016fac338406bfe2db3
zhangkailun <1248018887@qq.com>
2019-01-14 00:29:10 +0800

    编写header部分代码，出现显示问题，待解决

--- src/components/login.vue
+++ src/components/login.vue
@@ -3,11 +3,11 @@
         <div class="from-wrap">
             <h2>登录</h2>
             <Form ref="loginData" :model="loginData" :rules="ruleValidate" :label-width="80">
-                <FormItem label="账号" prop="acct">
-                    <Input type="text" v-model="loginData.acct" placeholder="请输入账号" />
+                <FormItem label="账号" prop="name">
+                    <Input type="text" v-model="loginData.name" placeholder="请输入账号" />
                 </FormItem>
-                <FormItem label="密码" prop="pass">
-                    <Input type="password" v-model="loginData.pass" placeholder="请输入密码" />
+                <FormItem label="密码" prop="password">
+                    <Input type="password" v-model="loginData.password" placeholder="请输入密码" />
                 </FormItem>
                 <FormItem class="form-footer">
                     <Button type="primary" @click="handleSubmit('loginData')">登录</Button>
@@ -28,11 +28,11 @@ export default {
                 pass: ''
             },
             ruleValidate: {
-                acct: [
+                name: [
                     { required: true, message: '账号不能为空', trigger: 'blur' },
                     { min: 3, max: 16, message: '账号长度3-16个字符', trigger: 'blur' }
                 ],
-                pass: [
+                password: [
                     { required: true, message: '密码不能为空', trigger: 'blur' },
                     { type: 'string', min: 6, max: 16, message: '密码长度6-16个字符', trigger: 'blur' }
                 ]
@@ -41,14 +41,13 @@ export default {
     },
     mounted () {
         this.getUserList() // 获取服务器端用户数据
-        this.postUserList() // 传递参数给后端服务器
+        // this.postUserList() // 传递参数给后端服务器
     },
     methods: {
         handleSubmit (name) {
             this.$refs[name].validate((valid) => {
                 if (valid) {
-                    this.$Message.success('登录成功!')
-                    this.$router.push('/pages/home') // 登录成功后跳转主页
+                    this.postUserList()
                 } else {
                     this.$Message.error('账号或密码错误!')
                 }
@@ -58,17 +57,16 @@ export default {
             this.$refs[name].resetFields()
         },
         postUserList () {
-            axios.post('http://47.110.78.180:3016/api/user/login', {
-                name: 'name',
-                password: '123456'
-            }).then(function (response) {
-                console.log(response)
+            let _this = this
+            axios.post('http://47.110.78.180:3016/api/user/login', this.loginData).then(function (response) {
+                _this.$router.push('/pages/home')
             }).catch(function (error) {
                 console.log(error)
+                _this.$Message.error('登录失败')
             })
         },
         getUserList () {
-            axios.get('http://47.110.78.180:3016/api/user?name=' + 'this.loginData.acct').then(res => {
+            axios.get(`http://47.110.78.180:3016/api/user?name=${this.loginData.name}`).then(res => {
                 console.log(res)
             }).catch(function (error) {
                 console.log(error)
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>welcome to home page</div>
+  <div>
+      <Homeheader class='header'></Homeheader>
+  </div>
 </template>
 
 <script>
+import Homeheader from './components/header'
 export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+        Homeheader
+    }
 }
 </script>
+
+<style>
+    .header{
+        height: 50px;
+        width: 100%;
+    }
+</style>
