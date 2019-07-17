5d5726ecee2ccc50469cebb132becc70beb3b57b
jiangronghua <613870505@qq.com>
2019-07-18 00:54:30 +0800

    代码优化

--- src/pages/patients/patientDetail.vue
+++ src/pages/patients/patientDetail.vue
@@ -30,11 +30,20 @@
                             <span>备注：{{pretreatment.is_prevent_diarrhea_remark}}</span>
                         </Col>
                     </Row>
+                    <div class="p-table">
+                        <div class="table-title" @click="addCombinationModal">
+                            <span>患者合并用药情况</span>
+                            <a class="p-title-btn">
+                                <Icon type="md-add" /><span>新增数据</span>
+                            </a>
+                        </div>
+                        <i-table border highlight-row :columns="combinationColumns" :data="combinationData"></i-table>
+                    </div>
                 </div>
                 <span v-else>暂无预处理</span>
             </div>
         </div>
-        <Modal v-model="addPretreatmentFlag" title="患者基本信息" :mask-closable="false" width="600">
+        <Modal v-model="addPretreatmentFlag" title="预处理信息" :mask-closable="false" width="600">
             <Form :model="pretreatmentFormItem" :label-width="100">
                 <Row>
                     <Col span="24">
@@ -80,6 +89,63 @@
                 <Button type="primary" @click="editPretreatmentOk" v-else>修改</Button>
            </div>
         </Modal>
+        <Modal v-model="addCombinationFlag" title="合并用药信息" :mask-closable="false" width="600">
+            <Form :model="combinationFormItem" :label-width="100">
+                <Row>
+                    <Col span="24">
+                        <FormItem label="药物：">
+                            <Input v-model="combinationFormItem.drug_name" placeholder="请输入药物名..."></Input>
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span="24">
+                        <FormItem label="用药时间：">
+                            <DatePicker format="yyyy/MM/dd" type="daterange" placeholder="请选择日期" v-model="combinationFormItem.date_range"></DatePicker>
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span="12">
+                        <FormItem label="用途：">
+                            <Input v-model="combinationFormItem.purpose" placeholder="请输入用途..."></Input>
+                        </FormItem>
+                    </Col>
+                    <Col span="12">
+                        <FormItem label="用量：">
+                            <Input v-model="combinationFormItem.dosage" placeholder="请输入用量..."></Input>
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span="12">
+                        <FormItem label="给药方式：">
+                            <Input v-model="combinationFormItem.drug_way" placeholder="请输入给药方式..."></Input>
+                        </FormItem>
+                    </Col>
+                    <Col span="12">
+                        <FormItem label="有无相互作用：">
+                            <RadioGroup v-model="combinationFormItem.is_interaction">
+                                <Radio :label="0">无</Radio>
+                                <Radio :label="1">有</Radio>
+                            </RadioGroup>
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span="24">
+                        <FormItem label="备注：">
+                            <Input v-model="combinationFormItem.remark" type="textarea" placeholder="请输入备注内容..."></Input>
+                        </FormItem>
+                    </Col>
+                </Row>
+            </Form>
+            <div slot="footer">
+                <Button type="text" @click="addCombinationFlag=false">取消</Button>
+                <Button type="primary" @click="addCombinationOk" v-if="addCombinationType == 'new'">确定</Button>
+                <Button type="primary" @click="editPretreatmentOk" v-else>修改</Button>
+           </div>
+        </Modal>
     </div>
 </template>
 <script>
@@ -97,7 +163,9 @@ export default {
             baseInfo: null,
             pretreatment: null,
             addPretreatmentFlag: false,
+            addCombinationFlag: false,
             addPretreatmentType: 'new',
+            addCombinationType: 'new',
             pretreatmentFormItem: {
                 patient_id: this.$route.params.id,
                 create_date: '',
@@ -106,14 +174,95 @@ export default {
                 is_prevent_diarrhea: 0,
                 is_prevent_diarrhea_remark: ''
             },
-            pretreatmentFormItemFormItemAdd: {
+            combinationFormItem: {
+                patient_id: this.$route.params.id,
+                drug_name: '',
+                date_range: [],
+                purpose: '',
+                dosage: '',
+                drug_way: '',
+                is_interaction: 0,
+                remark: ''
+            },
+            pretreatmentFormItemAdd: {
                 patient_id: this.$route.params.id,
                 create_date: '',
                 is_alkalization: 0,
                 is_alkalization_remark: '',
                 is_prevent_diarrhea: 0,
                 is_prevent_diarrhea_remark: ''
-            }
+            },
+            combinationFormItemAdd: {
+                patient_id: this.$route.params.id,
+                drug_name: '',
+                date_range: [],
+                purpose: '',
+                dosage: '',
+                drug_way: '',
+                is_interaction: 0,
+                remark: ''
+            },
+            combinationColumns: [
+                {
+                    title: '药物',
+                    key: 'drug_name',
+                    align: 'center'
+                },
+                {
+                    title: '开始时间',
+                    key: 'start_date',
+                    align: 'center'
+                },
+                {
+                    title: '结束时间',
+                    key: 'end_date',
+                    align: 'center'
+                },
+                {
+                    title: '用途',
+                    key: 'purpose',
+                    align: 'center'
+                },
+                {
+                    title: '用量',
+                    key: 'dosage',
+                    align: 'center'
+                },
+                {
+                    title: '给药方式',
+                    key: 'drug_way',
+                    align: 'center'
+                },
+                {
+                    title: '有无相互作用',
+                    key: 'is_interaction',
+                    align: 'center',
+                    render: (h, params) => {
+                        if (params.row.is_interaction) {
+                            return <span>有</span>
+                        } else {
+                            return <span>无</span>
+                        }
+                    }
+                },
+                {
+                    title: '备注',
+                    key: 'remark',
+                    align: 'center'
+                },
+                {
+                    title: '操作',
+                    key: 'action',
+                    align: 'center',
+                    render: (h, params) => {
+                        return <div class='ope-btns'>
+                            <a on-click={() => this.editBaseInfo(this, params)}>编辑</a>
+                            <a on-click={() => this.deletePatientModal(this, params)}>删除</a>
+                        </div>
+                    }
+                }
+            ],
+            combinationData: []
         }
     },
     methods: {
@@ -134,6 +283,9 @@ export default {
             axios.get(`${api.basehost}/patient/pretreatmentDetail/${id}`).then(function (res) {
                 if (res.data.code == 1) {
                     _this.pretreatment = res.data.data
+                    if (_this.pretreatment && _this.pretreatment.combinationList.length) {
+                        _this.combinationData = _this.pretreatment.combinationList
+                    }
                 } else {
                     _this.$Message.error(res.data)
                 }
@@ -143,8 +295,13 @@ export default {
         },
         addPretreatmentModal () {
             this.addPretreatmentType = 'new'
-            this.pretreatmentFormItem = this.pretreatmentFormItemFormItemAdd
+            this.pretreatmentFormItem = Object.assign({}, this.pretreatmentFormItemAdd)
             this.addPretreatmentFlag = true
+        },
+        addCombinationModal () {
+            this.addCombinationType = 'new'
+            this.combinationFormItem = Object.assign({}, this.combinationFormItemAdd)
+            this.addCombinationFlag = true
         },
         editPretreatmentModal () {
             this.addPretreatmentType = 'edit'
@@ -176,6 +333,26 @@ export default {
             }).catch(function (error) {
                 _this.$Message.error('新增预处理失败')
             })
+        },
+        addCombinationOk () {
+            let _this = this
+            if (this.combinationFormItem.date_range.length) {
+                this.combinationFormItem.start_date = this.combinationFormItem.date_range[0]
+                this.combinationFormItem.end_date = this.combinationFormItem.date_range[1]
+            } else {
+                this.combinationFormItem.start_date = ""
+                this.combinationFormItem.end_date = ""
+            }
+            axios.post(`${api.basehost}/patient/addCombination`, this.combinationFormItem).then(function (response) {
+                if (response.data.code == 1) {
+                    _this.addCombinationFlag = false
+                    _this.pretreatmentDetail(_this.$route.params.id)
+                } else {
+                    _this.$Message.error(response.data.msg)
+                }
+            }).catch(function (error) {
+                _this.$Message.error('新增预处理失败')
+            })
         }
     },
     mounted() {
@@ -187,9 +364,9 @@ export default {
 </script>
 <style scoped>
     .p-card .p-title {
-        line-height: 2;
+        line-height: 1.8;
         padding: 10px;
-        background-color: #eee;
+        background-color: #ccc;
     }
     .p-card .p-content {
         padding: 10px 0;
@@ -197,8 +374,22 @@ export default {
     }
     .p-title-btn {
         float: right;
+        margin-right: 20px;
     }
     .table-content {
         text-align: left;
+    }
+    .p-table {
+        margin-top: 20px;
+    }
+    .table-title {
+        text-align: center;
+        line-height: 40px;
+        border-right: 1px solid #e8eaec;
+        border-top: 1px solid #e8eaec;
+        border-left: 1px solid #e8eaec;
+    }
+    /deep/ .ope-btns a {
+        margin: 0 5px;
     }
 </style>
