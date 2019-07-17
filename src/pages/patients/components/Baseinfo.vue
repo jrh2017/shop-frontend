d45e007a952b3dbc583151ce59d65827e7abe6e5
jiangronghua <613870505@qq.com>
2019-07-17 20:14:46 +0800

    代码优化

--- src/pages/patients/components/Baseinfo.vue
+++ src/pages/patients/components/Baseinfo.vue
@@ -2,30 +2,88 @@
     <div class="b-content">
         <Row>
             <Col span="6">
-                <span>患者姓名: {{infoData.name}}</span>
+                <span>患者姓名：{{infoData.name}}</span>
             </Col>
             <Col span="6">
-                <span>床号: {{infoData.bed_no}}</span>
+                <span>床号： {{infoData.bed_no}}</span>
             </Col>
             <Col span="6">
-                <span>病历号/ID号: {{infoData.idcard}}</span>
+                <span>病历号/ID号： {{infoData.idcard}}</span>
             </Col>
             <Col span="6">
-                <span>性别: {{infoData.sex == 1 ? '男' : '女'}}</span>
+                <span>性别： {{infoData.sex == 1 ? '男' : '女'}}</span>
             </Col>
         </Row>
         <Row>
             <Col span="6">
-                <span>年龄: {{infoData.age}}</span>
+                <span>年龄： {{infoData.age}}</span>
             </Col>
             <Col span="6">
-                <span>身高: {{infoData.height}}cm</span>
+                <span>身高： {{infoData.height}}cm</span>
             </Col>
             <Col span="6">
-                <span>体重: {{infoData.weight}}kg</span>
+                <span>体重： {{infoData.weight}}kg</span>
             </Col>
             <Col span="6">
-                <span>体表面积: {{infoData.body_area}}㎡</span>
+                <span>体表面积： {{infoData.body_area}}㎡</span>
+            </Col>
+        </Row>
+        <Row>
+            <Col span="6">
+                <span>手机号码： {{infoData.phone}}</span>
+            </Col>
+            <Col span="6">
+                <span>临床诊断： {{infoData.clinical_diagnosis}}</span>
+            </Col>
+            <Col span="6">
+                <span>化疗史： {{infoData.chemotherapy_history}}</span>
+            </Col>
+            <Col span="6">
+                <span>过敏史： {{infoData.allergy_history}}</span>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <span class="title">危险因素：</span>
+                <div style="display: inline-block;" v-if="infoData.physical_power || infoData.pleural_effussion || infoData.loose_stools || infoData.infection || infoData.renal_disease || infoData.hepatosis || infoData.radiation || infoData.other">
+                    <span v-if="infoData.physical_power">体力状态≥3分、</span>
+                    <span v-if="infoData.pleural_effussion">胸腔积液或腹水、</span>
+                    <span v-if="infoData.loose_stools">便稀、</span>
+                    <span v-if="infoData.infection">存在感染、</span>
+                    <span v-if="infoData.renal_disease">肾功能障碍、</span>
+                    <span v-if="infoData.hepatosis">肝功能障碍、</span>
+                    <span v-if="infoData.radiation">放疗史、</span>
+                    <span v-if="infoData.other">其它({{infoData.other_desc}})</span>
+                </div>
+                <span v-else>无</span>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <span class="title">个体化用药相关基因检测：</span>
+                <div style="display: inline-block;" v-if="infoData.ugt1a1_28 || infoData.ugt1a1_6">
+                    <span v-if="infoData.ugt1a1_28">
+                        <template v-if="infoData.ugt1a1_28_type == 0">UGT1A1*28*1*1</template>
+                        <template v-else-if="infoData.ugt1a1_28_type == 1">UGT1A1*28*1*28</template>
+                        <template v-else>UGT1A1*28*28*28</template> &nbsp;
+                    </span>
+                    <span v-if="infoData.ugt1a1_6">
+                        <template v-if="infoData.ugt1a1_6_type == 0">UGT1A1*6*1*1</template>
+                        <template v-else-if="infoData.ugt1a1_6_type == 1">UGT1A1*6*1*6</template>
+                        <template v-else>UGT1A1*6*6*6</template>
+                    </span>
+                </div>
+                <span v-else>无</span>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <span class="title">β-葡萄糖醛酸酶活性检测：</span>
+                <span v-if="infoData.glucuronidase_undo">未做</span>
+                <div style="display: inline-block;" v-else>
+                    <span v-if="infoData.glucuronidase_blood">血清(浓度{{infoData.glucuronidase_blood_concentration}}) &nbsp;</span>
+                    <span v-if="infoData.glucuronidase_shit">粪便(浓度{{infoData.glucuronidase_shit_concentration}})</span>
+                </div>
             </Col>
         </Row>
     </div>
--- src/pages/patients/patientDetail.vue
+++ src/pages/patients/patientDetail.vue
@@ -1,9 +1,85 @@
 <template>
     <div class="p-detail-main">
         <div class="p-card">
-            <div class="title">基本信息</div>
+            <div class="p-title">基本信息</div>
             <Baseinfo :info-data="baseInfo" v-if="baseInfo != null"/>
         </div>
+        <div class="p-card">
+            <div class="p-title">预处理情况 
+                <i-button type="success" size="small" class="p-title-btn" v-if="!pretreatment" @click="addPretreatmentModal">添加预处理</i-button>
+                <i-button type="success" size="small" class="p-title-btn" v-if="pretreatment" @click="editPretreatmentModal">编辑预处理</i-button>
+            </div>
+            <div class="p-content">
+                <div v-if="pretreatment" class="table-content">
+                    <Row>
+                        <Col span="8">
+                            <span>日期：{{pretreatment.create_date}}</span>
+                        </Col>
+                        <Col span="8">
+                            <span>是否碱化：
+                                <template v-if="pretreatment.is_alkalization">是</template>
+                                <template v-else>否</template>
+                            </span> &nbsp;&nbsp;&nbsp;&nbsp; 
+                            <span>备注：{{pretreatment.is_alkalization_remark}}</span>
+                        </Col>
+                        <Col span="8">
+                            <span>是否预防腹泻：
+                                <template v-if="pretreatment.is_prevent_diarrhea">是</template>
+                                <template v-else>否</template>
+                            </span> &nbsp;&nbsp;&nbsp;&nbsp; 
+                            <span>备注：{{pretreatment.is_prevent_diarrhea_remark}}</span>
+                        </Col>
+                    </Row>
+                </div>
+                <span v-else>暂无预处理</span>
+            </div>
+        </div>
+        <Modal v-model="addPretreatmentFlag" title="患者基本信息" :mask-closable="false" width="600">
+            <Form :model="pretreatmentFormItem" :label-width="100">
+                <Row>
+                    <Col span="24">
+                        <FormItem label="日期：">
+                            <DatePicker type="date" placeholder="请选择日期" v-model="pretreatmentFormItem.create_date"></DatePicker>
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span="8">
+                        <FormItem label="是否碱化：">
+                            <RadioGroup v-model="pretreatmentFormItem.is_alkalization">
+                                <Radio :label="1">是</Radio>
+                                <Radio :label="0">否</Radio>
+                            </RadioGroup>
+                        </FormItem>
+                    </Col>
+                    <Col span="16">
+                        <FormItem label="备注：">
+                            <Input v-model="pretreatmentFormItem.is_alkalization_remark" placeholder="请输入备注..."></Input>
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span="8">
+                        <FormItem label="是否预防腹泻：">
+                            <RadioGroup v-model="pretreatmentFormItem.is_prevent_diarrhea">
+                                <Radio :label="1">是</Radio>
+                                <Radio :label="0">否</Radio>
+                            </RadioGroup>
+                        </FormItem>
+                    </Col>
+                    <Col span="16">
+                        <FormItem label="备注：">
+                            <Input v-model="pretreatmentFormItem.is_prevent_diarrhea_remark" placeholder="请输入备注..."></Input>
+                        </FormItem>
+                    </Col>
+                </Row>
+            </Form>
+            <div slot="footer">
+                <Button type="text" @click="addPretreatmentFlag=false">取消</Button>
+                <Button type="primary" @click="addPretreatmentOk" v-if="addPretreatmentType == 'new'">确定</Button>
+                <Button type="primary" @click="editPretreatmentOk" v-else>修改</Button>
+           </div>
+        </Modal>
     </div>
 </template>
 <script>
@@ -18,7 +94,26 @@ export default {
     data () {
         return {
             msg: 'test',
-            baseInfo: null
+            baseInfo: null,
+            pretreatment: null,
+            addPretreatmentFlag: false,
+            addPretreatmentType: 'new',
+            pretreatmentFormItem: {
+                patient_id: this.$route.params.id,
+                create_date: '',
+                is_alkalization: 0,
+                is_alkalization_remark: '',
+                is_prevent_diarrhea: 0,
+                is_prevent_diarrhea_remark: ''
+            },
+            pretreatmentFormItemFormItemAdd: {
+                patient_id: this.$route.params.id,
+                create_date: '',
+                is_alkalization: 0,
+                is_alkalization_remark: '',
+                is_prevent_diarrhea: 0,
+                is_prevent_diarrhea_remark: ''
+            }
         }
     },
     methods: {
@@ -33,18 +128,67 @@ export default {
             }).catch(function (error) {
                 _this.$Message.error('获取患者详情失败')
             })
+        },
+        pretreatmentDetail (id) {
+            let _this = this
+            axios.get(`${api.basehost}/patient/pretreatmentDetail/${id}`).then(function (res) {
+                if (res.data.code == 1) {
+                    _this.pretreatment = res.data.data
+                } else {
+                    _this.$Message.error(res.data)
+                }
+            }).catch(function (error) {
+                _this.$Message.error('获取患者详情失败')
+            })
+        },
+        addPretreatmentModal () {
+            this.addPretreatmentType = 'new'
+            this.pretreatmentFormItem = this.pretreatmentFormItemFormItemAdd
+            this.addPretreatmentFlag = true
+        },
+        editPretreatmentModal () {
+            this.addPretreatmentType = 'edit'
+            this.pretreatmentFormItem = this.pretreatment
+            this.addPretreatmentFlag = true
+        },
+        addPretreatmentOk () {
+            let _this = this
+            axios.post(`${api.basehost}/patient/addPretreatment`, this.pretreatmentFormItem).then(function (response) {
+                if (response.data.code == 1) {
+                    _this.addPretreatmentFlag = false
+                    _this.pretreatmentDetail(_this.$route.params.id)
+                } else {
+                    _this.$Message.error(response.data.msg)
+                }
+            }).catch(function (error) {
+                _this.$Message.error('新增预处理失败')
+            })
+        },
+        editPretreatmentOk () {
+            console.log('编辑预处理')
         }
     },
     mounted() {
         let id = this.$route.params.id
         this.patientDetail(id)
+        this.pretreatmentDetail(id)
     }
 }
 </script>
 <style scoped>
-    .p-card .title {
-        line-height: 1.4;
+    .p-card .p-title {
+        line-height: 2;
         padding: 10px;
         background-color: #eee;
+    }
+    .p-card .p-content {
+        padding: 10px 0;
+        text-align: center;
+    }
+    .p-title-btn {
+        float: right;
+    }
+    .table-content {
+        text-align: left;
     }
 </style>
